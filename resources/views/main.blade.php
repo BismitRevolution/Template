2ce27fd9afdb00f7ae7a1a79b17f98b16e557fe2
cymon1997 <cybermonster82@yahoo.com>
2018-07-09 23:07:29 +0700

    added foundation setting

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -14,11 +14,13 @@
         <!-- Normalize CSS -->
         <link rel="stylesheet" href="{{ asset('css/vendor/normalize.css') }}">
         <!-- Bootstrap -->
-        <link rel="stylesheet" href="{{ asset('css/vendor/bootstrap-v4.0.min.css') }}">
+        <!-- <link rel="stylesheet" href="{{ asset('css/vendor/bootstrap-v4.0.min.css') }}"> -->
         <!-- Foundation ZURB -->
-        <!-- <link rel="stylesheet" href="{{ asset('css/vendor/foundation.min.css') }}"> -->
+        <link rel="stylesheet" href="{{ asset('css/vendor/foundation.min.css') }}">
         <!-- Lightbox -->
         <!-- <link rel="stylesheet" href="{{ asset('css/vendor/lightbox.css') }}"> -->
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="{{ asset('css/vendor/fontawesome-all.css') }}">
         <!-- Basic Style -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <!-- Navbar Style -->
@@ -43,10 +45,10 @@
         <!-- JQuery -->
         <script type="application/javascript" src="{{ asset('js/vendor/jquery-3.2.1.min.js') }}"></script>
         <!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script> -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script> -->
 
         <!-- Bootstrap -->
-        <script type="application/javascript" src="{{ asset('js/vendor/bootstrap-v4.0.min.js') }}"></script>
+        <!-- <script type="application/javascript" src="{{ asset('js/vendor/bootstrap-v4.0.min.js') }}"></script> -->
         <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
         <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> -->
 
@@ -58,13 +60,13 @@
 
         <!-- Foundation ZURB -->
         <!-- <script type="application/javascript" src="{{ asset('js/vendor/what-input.js') }}"></script> -->
-        <!-- <script type="application/javascript" src="{{ asset('js/vendor/foundation.min.js') }}"></script> -->
+        <script type="application/javascript" src="{{ asset('js/vendor/foundation.min.js') }}"></script>
 
         <!-- Font Awesome -->
-        <script defer src="{{ asset('js/vendor/fontawesome-all.min.js') }}"></script>
+        <script type="application/javascript" src="{{ asset('js/vendor/fontawesome-all.js') }}"></script>
 
         <!-- Fred Carousel -->
-        <script type="application/javascript" src="{{ asset('js/vendor/jquery.carouFredSel.js') }}"></script>
+        <!-- <script type="application/javascript" src="{{ asset('js/vendor/jquery.carouFredSel.js') }}"></script> -->
 
         <!-- Customization -->
         <script type="application/javascript" src="{{ asset('js/_header.js') }}"></script>
