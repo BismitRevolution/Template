d1023c79a44226d0771daf031c19c06ba5122f6e
cymon1997 <cybermonster82@yahoo.com>
2018-10-04 23:22:17 +0700

    added materialize

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -15,6 +15,8 @@
         <link rel="stylesheet" href="{{ asset('css/vendor/normalize.css') }}">
         <!-- Bootstrap -->
         <!-- <link rel="stylesheet" href="{{ asset('css/vendor/bootstrap-v4.0.min.css') }}"> -->
+        <!-- Materialize -->
+        <!-- <link rel="stylesheet" href="{{ asset('css/vendor/materialize.min.css') }}"> -->
         <!-- Foundation ZURB -->
         <link rel="stylesheet" href="{{ asset('css/vendor/foundation.min.css') }}">
         <!-- Lightbox -->
@@ -58,6 +60,9 @@
         <!-- <script type="application/javascript" src="{{ asset('js/vendor/bootstrap-v4.0.min.js') }}"></script> -->
         <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
         <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> -->
+
+        <!-- Materialize -->
+        <!-- <script type="application/javascript" src="{{ asset('js/vendor/materialize.min.js') }}"></script> -->
 
         <!-- PopperJS -->
         <!-- <script type="application/javascript" src="https://unpkg.com/popper.js"></script> -->
